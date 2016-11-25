3ca5c9508b34e6238ae9958d760d40ec3538466f
Tom Nunn <t@tomnunn.uk>
2016-11-25 12:54:00 +0000

    working factory with constructor function

--- controllers.js
+++ controllers.js
@@ -3,22 +3,14 @@ toDoApp.controller('ToDoController', ['ToDoFactory', function(ToDoFactory) {
   var self = this;
 
   // set an attribute
-  self.todos = [
-    {task: "ToDo1", completed: true},
-    {task: "Learn Angular", completed: false},
-    {task:"Brush teeth", completed: false},
-    {task:"Have breakfast", completed: true}
-  ];
+  self.todos = [];
 
-  self.addToDo = function(task) {
-    self.todos.push(new ToDoFactory(task));
+  self.addToDo = function(toDoText) {
+    self.todos.push(new ToDoFactory(toDoText));
   };
 
   self.deleteToDo = function(index) {
-    self.todos.splice(index, 1);    
+    self.todos.splice(index, 1);
   };
-
-  self.complete = function(index) {
-    self.todos[index];
-  };
+  
 }]);
--- factories.js
+++ factories.js
@@ -1,23 +1,13 @@
 toDoApp.factory('ToDoFactory', function() {
 
-    function ToDo (toDoText) {
-        this.text = toDoText;
-        this.completed = false;
+    var Todo = function(toDoText) {
+      this.text = toDoText;
+      this.completed = false;
     }
-    
-    return ToDo
+
+    Todo.prototype.complete = function () {
+      this.completed = true;
+    };
+
+    return Todo;
 });
-//     return {
-//         addToDo: function(task) {
-//             var newTask = {task: task, completed: false};
-//             self.todos.push(newTask);
-//         },
-//         deleteToDo = function(index) {
-//             self.todos.splice(index, 1);
-//         },
-//         comlpeteToDo = function(index) {
-//             var selectedTodo = self.todos[index];
-//             selectedTodo.completed = true;
-//         }
-//     };
-// });
