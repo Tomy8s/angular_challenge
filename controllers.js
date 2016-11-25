bbf21d6ef8541043404a1129fa354be7347d843d
Tom Nunn <t@tomnunn.uk>
2016-11-25 11:23:01 +0000

    add expressions to controller

--- controllers.js
+++ controllers.js
@@ -1,10 +1,21 @@
 toDoApp.controller('ToDoController', function() {
 
+  var self = this;
+
   // set an attribute
-  this.todos = [
+  self.todos = [
     {task: "ToDo1", completed: true},
     {task: "Learn Angular", completed: false},
     {task:"Brush teeth", completed: false},
     {task:"Have breakfast", completed: true}
-  ]
+  ];
+
+  self.addToDo = function(task) {
+    var newTask = {task: task, completed: false};
+    self.todos.push(newTask);
+  };
+
+  self.deleteToDo = function(task) {
+    self.todos.splice(task, 1);
+  };
 });
